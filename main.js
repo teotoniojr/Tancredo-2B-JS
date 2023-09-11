ff8a210769fffdfdb43abe74de676f4fc4008984
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2023-09-11 10:45:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play()
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play()
 }
 
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+let contador = 0
+
+while (contador < listaDeTeclas.length){
+    
+    const tecla = listaDeTeclas[contador]
+    
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
