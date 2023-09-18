77974037e0137fbcae1961a2afb59ca454c4da91
Teotonio José de Souza Junior <102707827+teotoniojr@users.noreply.github.com>
2023-09-18 10:46:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,21 +4,26 @@ function tocaSom(idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll(".tecla")
 
-let contador = 0
 
-while (contador < listaDeTeclas.length){
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
     
     const tecla = listaDeTeclas[contador]
-    
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`
 
     listaDeTeclas[contador].onclick = function () {
         tocaSom(idAudio)
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        console.log(evento)
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+        tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
